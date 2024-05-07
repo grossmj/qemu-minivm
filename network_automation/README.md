c2abe1e56d53abea9897aaf7685b274b1b5e3a1d
Bernhard Ehlers <b-ehlers@example.com>
2024-05-07 11:14:49 +0200

    network_automation appliance ported from original Docker version
    - Updated to Ubuntu Noble
    - HashiCorp doesn't officially support Noble yet, use Mantic for now

---
+++ network_automation/README.md
@@ -0,0 +1,8 @@
+# Ubuntu - Network Automation Toolbox
+
+This appliance contains the following network automation tools:
+- Netmiko
+- NAPALM
+- Pyntc
+- Ansible
+- Terraform
---
+++ network_automation/etc/init.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Running in QEMU MiniVM
+if ! grep -q " /etc/hostname " /proc/mounts; then
+	# Uncomment if the module image is configured as a second drive
+	# mkdir -p /lib/modules && mount /dev/sdb /lib/modules
+	acpid
+	hostname "$(cat /etc/hostname)"
+	ip link set lo up
+	ifup -a -f
+fi
+
+cd
+exec bash -i -l
