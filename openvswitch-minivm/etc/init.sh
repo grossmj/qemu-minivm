6766dddf9eb3d7364ab518e44736ca2a4a1374a7
Bernhard Ehlers <b-ehlers@example.com>
2024-05-14 19:55:13 +0200

    openvswitch-minivm - IP configuration for bridge interfaces

--- openvswitch-minivm/etc/init.sh
+++ openvswitch-minivm/etc/init.sh
@@ -10,8 +10,11 @@ if ! grep -q " /etc/hostname " /proc/mounts; then
 	ip link set lo up
 	sed -n 's/^ *\(eth[0-9]*\): .*/\1/p' /proc/net/dev | sort -V | \
 		while read -r intf; do ip link set dev "$intf" up; done
+	/etc/openvswitch/init.sh
 	ifup -a -f
+else
+	/etc/openvswitch/init.sh
+	for intf in br0 br1 br2 br3; do ifup -f "$intf"; done
 fi
 
-/etc/openvswitch/init.sh
 exec bash -i
